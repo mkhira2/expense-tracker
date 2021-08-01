8a83d4c84e38bb7fbc17ba9fbadc1d1d880d600f
Kenji Hirabayashi <mkhira2@gmail.com>
2021-07-31 19:55:34 -0500

    adds expense form inputs

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import React from 'react'
+import React from 'react';
 
-import Expenses from './components/Expenses/Expenses'
+import NewExpense from './components/NewExpense/NewExpense';
+import Expenses from './components/Expenses/Expenses';
 
 const App = () => {
   const expenses = [
@@ -33,6 +34,7 @@ const App = () => {
   return (
     <div>
       <h2>Let's get started!</h2>
+      <NewExpense />
       <Expenses items={expenses} />
     </div>
   );
